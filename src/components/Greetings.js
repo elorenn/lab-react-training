4f2efdb2cf0fb4a377b931078a82dad7d5e37969
Lorena Sarasua <lorena.sarasua@gmail.com>
2022-10-11 03:50:21 -0400

    replaced selectLang function with a language Object

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -3,24 +3,16 @@ import React from 'react';
 function Greetings(props) {
   const { lang } = props;
 
-  function selectLang(lang) {
-    if (lang === 'de') {
-      return <span>Hallo </span>;
-    }
-    if (lang === 'en') {
-      return <span>Hello </span>;
-    }
-    if (lang === 'es') {
-      return <span>Hola </span>;
-    }
-    if (lang === 'fr') {
-      return <span>Bonjour </span>;
-    }
-  }
+  let greetings = {
+    de: 'Hallo',
+    en: 'Hello',
+    fr: 'Bonjour',
+    es: 'Hola',
+  };
 
   return (
     <div className="Greetings IdCard">
-      {selectLang(lang)} {props.children}
+      {greetings[props.lang]} {props.children}
     </div>
   );
 }
